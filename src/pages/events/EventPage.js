a2bc4177110ebc8ffbfed534848daa08fe5cc7a9
Yari-Carelli <yaricarelli@hotmail.it>
2022-12-16 15:55:04 +0000

    Create EventPage

---
+++ src/pages/events/EventPage.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { axiosReq } from '../../api/axiosDefaults';
+import Event from './Event';
+
+
+function EventPage() {
+  const { id } = useParams();
+  const [event, setEvent] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: event }] = await Promise.all([
+          axiosReq.get(`/events/${id}`),
+        ]);
+        setEvent({ results: [event] });
+      } catch (err) {
+        // console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
+
+  return (
+    <Container>
+      <Event {...event.results[0]} setEvent={setEvent} eventPage />
+    </Container>
+  );
+}
+
+export default EventPage;
